1864448b6831e4404fc0a992457fb54a9248a52f
Your Name <you@example.com>
2025-03-27 18:54:36 +0300

    checked even number fuction

---
+++ numbers.test.js
@@ -0,0 +1,8 @@
+// const { default: expect } = require("expect");
+// const { default: expect } = require("expect");
+const { default: expect } = require('expect')
+const {isEven} = require('./numbers')
+
+test('test if 8 is even', ()=> {
+    expect(isEven(8)).toBe(true)
+})
